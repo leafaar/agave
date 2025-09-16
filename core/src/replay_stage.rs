62a7feb518ce1964ca3138d07adea53b18dd0998
leafaar <yyyyrrzb@gmail.com>
2025-09-16 11:24:00 -0300

    chore(replay): remove pin threads

--- core/src/replay_stage.rs
+++ core/src/replay_stage.rs
@@ -609,7 +609,7 @@ impl ReplayStage {
             block_commitment_cache.clone(),
             rpc_subscriptions.clone(),
         );
-        let mut run_replay = move || {
+        let run_replay = move || {
             let verify_recyclers = VerifyRecyclers::default();
             let _exit = Finalizer::new(exit.clone());
             let mut identity_keypair = cluster_info.keypair().clone();
@@ -1227,16 +1227,7 @@ impl ReplayStage {
         };
         let t_replay = Builder::new()
             .name("solReplayStage".to_string())
-            .spawn(move || {
-                // Pin main replay thread to core 1 (core 0 is reserved for POH)
-                if let Some(cores) = core_affinity::get_core_ids() {
-                    if cores.len() > 1 {
-                        core_affinity::set_for_current(cores[1]);
-                        info!("Pinned main replay thread to core {:?}", cores[1]);
-                    }
-                }
-                run_replay()
-            })
+            .spawn(run_replay)
             .unwrap();
 
         Ok(Self {
